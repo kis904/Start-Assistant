dfa9198cd975b531a2feb879359f8dacd83a9d88
kis904 <danielsipos97@gmail.com>
2026-07-03 11:57:11 +0200

    Added config file, buttons, pop up window
    I have a window a few buttons and a pop up window. My first and only feature yet is that it can change the size of the window based on user input and changes it's config file that it handles itself.
    It was a hell a lot of work to make that config text file work... It always had some problem e.g. when it was writing the new settings it wanted string not list, or the \n that signed a new line stayed there so it didn't recognize the settings in the conf file or couldn't apply it.
    To sum it up, it was a lot of time, but now I will work on its main feature.

--- assistant.py
+++ assistant.py
@@ -1,5 +1,21 @@
 import tkinter as tk
+from tkinter import*
+from tkinter import ttk
 from PIL import Image, ImageTk
+import os, pynput, pyautogui, pytesseract
+from PIL import Image
+
+#open config file
+print("A")
+config=open("config.txt", "r+", encoding="UTF-8")
+lines=config.readlines()
+for line in lines:
+    line.strip()
+    sp=line.split(" : ")
+    print(sp)
+    if sp[0]=="dimensions":
+        dim=sp[1].strip()
+
 
 def back_ground(window, image_path):
     image=Image.open(image_path)
@@ -9,7 +25,93 @@ def back_ground(window, image_path):
     background_label.image=photo
     background_label.place(relwidth=1, relheight=1)
 
-window=tk.Tk('Start Assistant')
-window.title='Start Assistant'
+def findImg(img_path, minSearchTime, confidence):
+    img=Image.open(img_path)
+    location=pyautogui.locateOnScreen(image=img, minSearchTime=minSearchTime, confidence=confidence)
+    center=pyautogui.center(location)
+    x, y=center
+    return x, y
+
+window=tk.Tk()
+window.title("Start Assistant")
+try:
+    window.geometry(dim)
+except:
+    window.geometry("1080x720")
 back_ground(window, 'C:/dev/startassist/start.jpg')
+
+
+#def enter(event):
+#    event.widget.config(style=)
+
+def Macondo():
+    os.startfile("C:/Program Files/Mozilla Firefox/firefox.exe")
+    url="https://macondo.hackclub.com/"
+    #if findImg("C:/dev/startassist/searchbar.png", 10, 0.5):
+    #    pyautogui.typewrite(url)
+    #    pyautogui.press('Enter')
+
+def gmail():
+    os.startfile("C:/Program Files/Mozilla Firefox/firefox.exe")
+    url="https://gmail.com/"
+    if findImg("C:/dev/startassist/searchbar.png", 3, 0.5):
+        pyautogui.hotkey("ctrl", "l")
+        pyautogui.typewrite(url)
+        pyautogui.press('Enter')
+
+def overwrite(param, conf):
+    with open("config.txt", "w") as file:
+        new=[]
+        print(lines)
+        for line in lines:
+            if line!=param:
+                new.append(line)
+        new.append(f"dimensions : {conf}\n")
+        print(*new)
+        file.writelines(new)
+
+def save_config(param, widget, win):
+    print("B")
+    conf=widget.get("1.0", "end-1c")
+    containes=False
+    if param=="dimensions":
+        print("C")
+        for line in lines:
+            print("D")
+            sp=line.split(" : ")
+            print(sp)
+            if sp[0]=="dimensions":
+                overwrite(f"{sp[0]} : {sp[1]}", conf)
+                containes=True
+        if containes==False:
+            config.write(f"\ndimensions : {conf}")
+    win.destroy()
+
+def chg_dim():
+    chgwin=Toplevel(window, takefocus=True)
+    label=Label(chgwin, text="Write here the new dimensions \n in widthxheight format e.g 1080x720")
+    dimensions=Text(chgwin, height=50, width=100)
+    save=Button(chgwin, width=50, height=25, bg="green", command=lambda: save_config("dimensions", dimensions, chgwin))
+    label.pack()
+    dimensions.pack()
+    save.pack()
+
+buttons=[
+    ("button_1", "Gmail"),
+    ("button_2", "Macondo")
+]
+
+def ex():
+    window.quit()
+
+buttons1=[
+    #["change_dim","Change dimensions of the window", chg_dim()],
+    (4,5,6)
+]
+for name, title, command in buttons1:
+    iterbutton=ttk.Button(window, text=title, command=lambda: command)
+    iterbutton.pack()
+button = ttk.Button(window, text="test", command=lambda: chg_dim())
+button.pack()
+
 window.mainloop()
