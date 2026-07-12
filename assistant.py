3c22a63ce50e89f90a718425ecc08ea89a8aa9f8
kis904 <danielsipos97@gmail.com>
2026-07-12 18:41:30 +0200

    Added new feature, working on a new one
    In today's developing session I successfully added a new feature that let the user change the background picture, unfortunately it doesn't work with accented letter (áéőúűóüöí).
    In addition, the mouse movement tracking works, but freezes the app with the hundreds of called functions per sec if the mouse is moving, but the mechanism that detects and logs where the user clicked works fine. And also finally got how to store functions in a way that it's not called (just had to remove the () after it)

--- assistant.py
+++ assistant.py
@@ -1,21 +1,30 @@
 import tkinter as tk
 from tkinter import*
-from tkinter import ttk
+from tkinter import ttk, filedialog
 from PIL import Image, ImageTk
-import os, pynput, pyautogui, pytesseract
+import os, pyautogui, pytesseract
 from PIL import Image
+from time import sleep, time
+import webbrowser
+from pynput import mouse, keyboard
+from tkinter.messagebox import showinfo
 
 #open config file
-print("A")
+#print("A")
 config=open("config.txt", "r+", encoding="UTF-8")
 lines=config.readlines()
 for line in lines:
     line.strip()
     sp=line.split(" : ")
-    print(sp)
+    #print(sp)
     if sp[0]=="dimensions":
         dim=sp[1].strip()
+    if sp[0]=="background":
+        background_route=sp[1]
 
+#creating variables that will be used later
+stop_track=False
+mouse_moved=0
 
 def back_ground(window, image_path):
     image=Image.open(image_path)
@@ -38,63 +47,125 @@ try:
     window.geometry(dim)
 except:
     window.geometry("1080x720")
-back_ground(window, 'C:/dev/startassist/start.jpg')
+try:
+    back_ground(background_route)
+except:
+    back_ground(window, 'C:/dev/startassist/start.jpg')
 
+buttonhoverstyle=ttk.Style()
+buttonhoverstyle.configure("buttonStyle.TButton",
+                         width=20,
+                         height=5,
+                         background="#98BBE9")
 
 #def enter(event):
 #    event.widget.config(style=)
 
 def Macondo():
     os.startfile("C:/Program Files/Mozilla Firefox/firefox.exe")
-    url="https://macondo.hackclub.com/"
-    #if findImg("C:/dev/startassist/searchbar.png", 10, 0.5):
-    #    pyautogui.typewrite(url)
-    #    pyautogui.press('Enter')
+    start_mouse_track()
+    
+def start_mouse_track():
+    print("A")
+    mouse_listener=mouse.Listener(
+        on_click=mouse_track,
+        on_move=mouse_move
+    )
+    mouse_listener.start()
+    s=0
+    while s<=10:
+        if stop_track==True:
+            mouse_listener.stop()
+            #mouse_listener.join()
+            print("E")
+            s+=1
+            sleep(1)
+            return
+    mouse_listener.stop()
+    #mouse_listener.join()
+
+def mouse_track(x, y, button, pressed):
+    if button==mouse.Button.left and pressed:
+        print("mouse button pressed", pressed, button)
+        with open("map.txt", "a+") as file:
+            file.write(f"click : {x};{y}\n")
+
+def mouse_move(x, y):
+    global mouse_moved
+    if mouse_moved+3==int(time()):
+        global stop_track
+        stop_track=True
+        print("X")
+    else:
+        mouse_moved=int(time())
 
 def gmail():
-    os.startfile("C:/Program Files/Mozilla Firefox/firefox.exe")
+    #webbrowser.open("gmail.com")
+    webbrowser.open("gmail.com")
+    #os.startfile("C:/Program Files/Mozilla Firefox/firefox.exe")
     url="https://gmail.com/"
-    if findImg("C:/dev/startassist/searchbar.png", 3, 0.5):
+    if findImg("screenshots/searchbar.png", 3, 0.5):
         pyautogui.hotkey("ctrl", "l")
+        sleep(0.5)
         pyautogui.typewrite(url)
         pyautogui.press('Enter')
 
-def overwrite(param, conf):
+def overwrite(param, param2, conf):
     with open("config.txt", "w") as file:
         new=[]
-        print(lines)
+        target=f"{param} : {param2}"
+        #print(lines)
         for line in lines:
-            if line!=param:
+            if line!=target:
                 new.append(line)
-        new.append(f"dimensions : {conf}\n")
-        print(*new)
+        new.append(f"{param} : {conf}\n")
         file.writelines(new)
+        return
 
-def save_config(param, widget, win):
-    print("B")
-    conf=widget.get("1.0", "end-1c")
+def save_config(param, **kwargs):
     containes=False
     if param=="dimensions":
-        print("C")
+        widget=kwargs.get("widget")
+        win=kwargs.get("win")
+        conf=widget.get("1.0", "end-1c")
         for line in lines:
-            print("D")
             sp=line.split(" : ")
-            print(sp)
+            #print(sp)
             if sp[0]=="dimensions":
-                overwrite(f"{sp[0]} : {sp[1]}", conf)
+                overwrite(sp[0], sp[1], conf)
                 containes=True
         if containes==False:
             config.write(f"\ndimensions : {conf}")
-    win.destroy()
+        win.destroy()
+    elif param=="background":
+        conf=kwargs.get("conf")
+        for line in lines:
+            sp=line.split(" : ")
+            #print(sp)
+            if sp[0]=="background":
+                overwrite(sp[0], sp[1], conf)
+                containes=True
+        if containes==False:
+            config.write(f"background : {conf}\n")
+    showinfo("Changes in config", "Saved changes successfully in config.txt")
 
 def chg_dim():
     chgwin=Toplevel(window, takefocus=True)
     label=Label(chgwin, text="Write here the new dimensions \n in widthxheight format e.g 1080x720")
     dimensions=Text(chgwin, height=50, width=100)
-    save=Button(chgwin, width=50, height=25, bg="green", command=lambda: save_config("dimensions", dimensions, chgwin))
+    save=Button(chgwin, width=50, height=25, bg="green", command=lambda: save_config(param="dimensions", widget=dimensions, win=chgwin))
     label.pack()
     dimensions.pack()
     save.pack()
+
+def change_background():
+    chgwin=Toplevel(window, takefocus=True)
+    label=Label(chgwin, text="Select the new background \n supported file format is jpg")
+    label.pack()
+    background=filedialog.askopenfile(title="Select the new background", filetypes=[("JPEG", "*.jpg")])
+    background=background.name
+    save_config(param="background", win=chgwin, conf=background)
+    print(background)
 
 buttons=[
     ("button_1", "Gmail"),
@@ -105,13 +176,14 @@ def ex():
     window.quit()
 
 buttons1=[
-    #["change_dim","Change dimensions of the window", chg_dim()],
-    (4,5,6)
+    ["change_dim","Change dimensions of the window", chg_dim],
+    ["change_back", "Change background", change_background],
+    ["gmail", "Gmail", gmail],
+    ["macondo", "Macondo", Macondo],
+    ["exit", "Exit", ex]
 ]
 for name, title, command in buttons1:
-    iterbutton=ttk.Button(window, text=title, command=lambda: command)
+    iterbutton=ttk.Button(window, text=title, command=command)
     iterbutton.pack()
-button = ttk.Button(window, text="test", command=lambda: chg_dim())
-button.pack()
 
 window.mainloop()
